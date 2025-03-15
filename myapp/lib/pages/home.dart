9fa21d883aed2d15e98739c3833b176cb3420077
Augusto <augustotsantos@outlook.com>
2025-03-15 12:49:11 -0300

    add nova fonte

--- myapp/lib/pages/home.dart
+++ myapp/lib/pages/home.dart
@@ -24,6 +24,7 @@ class _HomeState extends State<Home> {
             fontWeight: FontWeight.bold,
             letterSpacing: 2,
             color: Colors.grey,
+            fontFamily: 'IndieFlower',
           ),
         ),
       ),
